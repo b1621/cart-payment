59cc881ccc4e779a8ad3e7541e8b21a0ce1a0e55
b1 <bewuket1621@gmail.com>
2023-12-10 18:09:42 +0300

    calculating total and amount works

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 import Navbar from "./components/Navbar";
 import CartContainer from "./components/CartContainer";
+import { calculateTotals } from "./features/cart/cartSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
 
 function App() {
+  const dispatch = useDispatch();
+  const { cartItems } = useSelector((state) => state.cart);
+  useEffect(() => {
+    dispatch(calculateTotals());
+  }, [cartItems]);
   return (
     <main>
       <Navbar />
--- src/features/cart/cartSlice.jsx
+++ src/features/cart/cartSlice.jsx
@@ -3,7 +3,7 @@ import cartItems from "../../cartItems";
 
 const initialState = {
   cartItems: cartItems,
-  amount: 4,
+  amount: 0,
   total: 0,
   isLoading: true,
 };
@@ -28,9 +28,21 @@ const cartSlice = createSlice({
       const cartItem = state.cartItems.find((item) => item.id === payload.id);
       cartItem.amount = cartItem.amount - 1;
     },
+    calculateTotals: (state) => {
+      let amount = 0;
+      let total = 0;
+      state.cartItems.forEach((item) => {
+        amount += item.amount;
+        total += item.amount * item.price;
+      });
+
+      state.amount = amount;
+      state.total = total;
+    },
   },
 });
 
 // console.log("cart slice = ", cartSlice);
-export const { clearCart, removeItem, increase, decrease } = cartSlice.actions;
+export const { clearCart, removeItem, increase, decrease, calculateTotals } =
+  cartSlice.actions;
 export default cartSlice.reducer;
